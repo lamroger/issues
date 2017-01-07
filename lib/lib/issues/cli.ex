812c8909b0aa5eadda3888281710c444f5cb8699
Roger Lam <mrlamroger@gmail.com>
2017-01-06 17:48:44 -0800

    parse and sort issues from github

--- lib/lib/issues/cli.ex
+++ lib/lib/issues/cli.ex
@@ -8,7 +8,9 @@ defmodule Issues.CLI do
   """
 
   def run(argv) do
-    parse_args(argv)
+    argv
+    |> parse_args
+    |> process
   end
 
   @doc """
@@ -30,11 +32,43 @@ defmodule Issues.CLI do
     { _, [ user, project, count ], _}
       -> { user, project, String.to_integer(count) }
 
-    { _, [ user, project], _ }
+    { _, [ user, project ], _ }
       -> { user, project, @default_count }
 
     _ -> :help
 
     end
+  end
+
+  def process(:help) do
+    IO.puts """
+    usage: issues <user> <project> [ count | #{@default_count} ]
+    """
+    System.halt(0)
+  end
+
+  def process({ user, project, _count }) do
+    Issues.GithubIssues.fetch(user, project)
+    |> decode_response
+    |> convert_to_list_of_maps
+    |> sort_into_ascending_order
+  end
+
+  def decode_response({:ok, body}), do: body
+
+  def decode_response({:error, error}) do
+    {_, message} = List.keyfind(error, "message", 0)
+    IO.puts "Error fetching from Github: #{message}"
+    System.halt(2)
+  end
+
+  def convert_to_list_of_maps(list) do
+    list
+    |> Enum.map(&Enum.into(&1, Map.new))
+  end
+
+  def sort_into_ascending_order(list_of_issues) do
+    Enum.sort list_of_issues,
+              fn i1, i2 -> i1["created_at"] <= i2["created_at"] end
   end
 end
